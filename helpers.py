0d75fc0a927c3e748a67bbbd1e35c1e79fb08dc9
Pudro <ptr.pustelnik@gmail.com>
2023-06-04 12:41:55 +0200

    Changed visualize_data function, moving some logic to show_image_from_np

--- helpers.py
+++ helpers.py
@@ -1,11 +1,11 @@
 import os
 import rasterio
 import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
 from matplotlib.colors import ListedColormap
 import matplotlib.patches as mpatches
 import random
 import numpy as np
+
 
 class SurfaceMapping:
     def __init__(self):
@@ -22,7 +22,7 @@ class SurfaceMapping:
             'Hidden Median': 10,
             'Hidden Road': 11
         }
-        
+
         self.mapping_names_colors = {
             1: ('Alley', 'green'),
             2: ('Paved Drive', 'purple'),
@@ -36,10 +36,27 @@ class SurfaceMapping:
             10: ('Hidden Median', 'orange'),
             11: ('Hidden Road', 'white'),
         }
-        
 
-def visualize_data(dataset_dir, show_masks=True, max_samples=5, shuffle=True):
+def show_image_from_np(photo=None, mask=None, figsize=(10, 10)):
+    if photo is not None or mask is not None:
+        plt.figure(figsize=figsize)
+    if photo is not None:
+        plt.imshow(photo)
+    if mask is not None:
+        unique_values = np.unique(mask)
+        unique_values = unique_values[unique_values > 0]
+        mask[np.where(mask == 0)] = np.nan
+        color_map = ListedColormap([SurfaceMapping().mapping_names_colors[pos][1] for pos in range(1, 12)])
+        plt.imshow(mask, alpha=0.5, cmap=color_map, vmin=1, vmax=11)
+        legend_handles = [mpatches.Patch(color=SurfaceMapping().mapping_names_colors[value][1],
+                                         label=SurfaceMapping().mapping_names_colors[value][0])
+                          for value in unique_values]
+        plt.legend(handles=legend_handles, title='Pixel Values')
+    if photo is not None or mask is not None:
+        plt.show()
 
+
+def visualize_data(dataset_dir, show_masks=True, max_samples=5, shuffle=True, labels=None):
     photo_folder = os.path.join(dataset_dir, "photos")
     mask_folder = os.path.join(dataset_dir, "masks")
 
@@ -47,11 +64,10 @@ def visualize_data(dataset_dir, show_masks=True, max_samples=5, shuffle=True):
 
     if shuffle:
         random.shuffle(file_list)
-        
+
     cnt = 0
     for file_name in file_list:
         if file_name.startswith("photo") and cnt < max_samples:
-            plt.figure(figsize=(10,10))
             mask_file_name = "mask_" + file_name[6:-4] + ".tif"
 
             photo_path = os.path.join(photo_folder, file_name)
@@ -59,25 +75,11 @@ def visualize_data(dataset_dir, show_masks=True, max_samples=5, shuffle=True):
 
             with rasterio.open(photo_path) as photo_ds:
                 # photo = photo_ds.read([1,2,3])  # Assuming single band image
-                photo = np.transpose(photo_ds.read([1,2,3]), (1, 2, 0)) # Load n layers
-                plt.imshow(photo)
+                photo = np.transpose(photo_ds.read([1, 2, 3]), (1, 2, 0))  # Load n layers
 
             if show_masks:
                 with rasterio.open(mask_path) as mask_ds:
                     mask = mask_ds.read(1)  # Assuming single band mask
-                    print(np.unique(mask))
-                    unique_values = np.unique(mask)
-                    unique_values = unique_values[unique_values>0]
-                    print(unique_values)
-                    mask[np.where(mask == 0)] = np.nan
-                    
-                    color_map = ListedColormap([SurfaceMapping().mapping_names_colors[pos][1] for pos in range(1,12)])
 
-                    plt.imshow(mask, alpha=0.5, cmap=color_map, vmin=1, vmax=11)
-                    
-                    legend_handles = [mpatches.Patch(color=SurfaceMapping().mapping_names_colors[value][1], label=SurfaceMapping().mapping_names_colors[value][0]) for value in unique_values]
-
-                    plt.legend(handles=legend_handles, title='Pixel Values')
-
-            plt.show()
+            show_image_from_np(photo, mask)
             cnt += 1
